4b24158303ca904cd089d6d501028aae414cc16a
JJv03 <106931262+JJv03@users.noreply.github.com>
2023-05-24 12:37:42 +0200

    Add files via upload

--- firewall.sh
+++ firewall.sh
@@ -22,8 +22,8 @@ iptables -t nat -A POSTROUTING -s 192.168.52.0/24 -o enp0s3 -j MASQUERADE   #Red
 iptables -t nat -A POSTROUTING -s 192.168.53.0/24 -o enp0s3 -j MASQUERADE   #Red Interna 3
 
 #Todo lo que salga fuera de extranet tendrá la IP de debian1
-iptables -t nat -A POSTROUTING -o enp0s3 -j SNAT --to 192.168.50.2  #IP debian1
-iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.50.2  #IP debian1
+iptables -t nat -A POSTROUTING -o enp0s3 -j SNAT --to 192.168.56.2  #IP debian1
+iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.56.2  #IP debian1
 
 #Se redireccionan las peticiones desde NAT, si es al servidor web de debian2 o al servidor ssh de debian5
 iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.51.2:80  #RI1
@@ -46,7 +46,7 @@ iptables -A FORWARD -p tcp --dport 80 -d 192.168.51.2 -j ACCEPT
 iptables -A FORWARD -p tcp --dport 22 -d 192.168.53.2 -j ACCEPT
 
 #Se permite la entrada de todo el tráfico local (intranet)
-iptables -A INPUT -i lo -p all -j ACCEPT        #Loopback
+iptables -A INPUT -i lo -j ACCEPT        #Loopback
 iptables -A INPUT -i enp0s9 -p all -j ACCEPT    #RI1
 iptables -A INPUT -i enp0s10 -p all -j ACCEPT   #RI2
 
